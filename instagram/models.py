52797a31d3dc257cf0e61b3592e798f6835414cc
kimani-njoroge <kimani064@gmail.com>
2018-07-22 00:34:30 +0300

    craete profile view

--- instagram/models.py
+++ instagram/models.py
@@ -8,7 +8,7 @@ from tinymce.models import HTMLField
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profpic/')
     bio = models.TextField(max_length=100)
-    insta_user = models.ForeignKey(User)
+    user = models.ForeignKey(User)
 
     def __str__(self):
         return self.insta_user
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import ProfileForm
+from .models import Image,Profile
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+
+def profile(request):
+
+    return render(request,'profile.html')
