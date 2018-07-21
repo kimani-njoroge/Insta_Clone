e8da438761c447cfe9674c613776d3f92ce39d3a
kimani-njoroge <kimani064@gmail.com>
2018-07-21 23:37:27 +0300

    create profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profpic/')
+    bio = models.TextField(max_length=100)
+    insta_user = models.ForeignKey(User)
