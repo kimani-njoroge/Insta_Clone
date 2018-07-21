23be9881d35cfad40b16044e6a59d6af467ddd19
kimani-njoroge <kimani064@gmail.com>
2018-07-22 01:39:35 +0300

    create image form to post images

--- instagram/forms.py
+++ instagram/forms.py
@@ -6,3 +6,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['insta_user']
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile']
