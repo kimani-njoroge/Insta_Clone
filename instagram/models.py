29a10800ce6bcdcbb32c43dd5ef82d4c75dc57e4
kimani-njoroge <kimani064@gmail.com>
2018-07-21 23:51:59 +0300

    craete image model

--- instagram/models.py
+++ instagram/models.py
@@ -8,3 +8,16 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profpic/')
     bio = models.TextField(max_length=100)
     insta_user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.insta_user
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length=30)
+    image_caption = models.TextField(max_length=100)
+    profile = models.ForeignKey(Profile)
+
+    def __str__(self):
+        self.image_name
