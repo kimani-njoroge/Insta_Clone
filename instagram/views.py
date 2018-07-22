fe54c771d1beb4d7602f7fc2b38195b3efdea82d
kimani-njoroge <kimani064@gmail.com>
2018-07-23 00:16:52 +0300

    add search result view

--- instagram/views.py
+++ instagram/views.py
@@ -59,3 +59,11 @@ def addimages(request):
 #     return render(request,'imagedetails.html',{"images":images})
 
 
+@login_required(login_url='/accounts/login/')
+def search_result(request):
+    if 'query' in request.GET and request.GET['query']:
+        query = request.GET.get("query")
+        user = Profile.profile_search(query)
+        return render(request,'search.html',{"user":user})
+    else:
+        return render(request,'search.html')
