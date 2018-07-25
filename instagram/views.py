b4d05777e33fe2520ee4de3b7e80de05426a7e1a
kimani-njoroge <kimani064@gmail.com>
2018-07-25 15:44:09 +0300

    display a single users post

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import render, redirect
+from friendship.exceptions import AlreadyExistsError
+
 from .forms import ProfileForm,ImageForm
 from .models import Image,Profile
 from friendship.models import Friend, Follow,Block
@@ -9,17 +11,17 @@ from friendship.models import Friend, Follow,Block
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    current_user = request.user
+    # current_user = request.user
     images = Image.objects.all()
     profile = Profile.objects.all()
-    user = Profile.objects.get(user=current_user)
-    print(user)
-    return render(request,'index.html',{"images":images,"profile":profile,"user":user})
+    # user = Profile.objects.get(user=current_user)
+    # print(user)
+    return render(request,'index.html',{"images":images,"profile":profile})
 
 @login_required(login_url='/accounts/login/')
 def addprofile(request):
     current_user = request.user
-    profile = Profile.objects.filter(user_id=current_user)
+    # profile = Profile.objects.filter(user_id=current_user)
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -28,7 +30,7 @@ def addprofile(request):
             profile.save()
     else:
         form = ProfileForm()
-    return render(request, 'profile/newprofile.html', {"form":form,"profile":profile})
+    return render(request, 'profile/newprofile.html', {"form":form})
 
 
 @login_required(login_url='/accounts/login/')
@@ -37,7 +39,20 @@ def showprofile(request, user_id):
     profile = Profile.objects.get(user=users)
     followers = len(Follow.objects.followers(users))
     following = len(Follow.objects.following(users))
-    return render(request, 'profile/profile.html',{"profile":users,"user":profile,"followers":followers,"following":following,"follow":follow})
+    images = Image.objects.filter(author=users)
+    print("showing")
+    return render(request, 'profile/profile.html',{"profile":users,"user":profile,"followers":followers,"following":following,"follow":follow,"images":images})
+
+
+@login_required(login_url='/accounts/login/')
+def follow(request,user_id):
+    users = User.objects.get(id=user_id)
+    try:
+        follow = Follow.objects.add_follower(request.user, users)
+    except AlreadyExistsError:
+        return Http404
+    # print("followed")
+    return redirect('/showprofile/'+user_id)
 
 
 @login_required(login_url='/accounts/login/')
@@ -68,13 +83,6 @@ def showdetails(request,profile_id):
         raise Http404()
     return render(request,'imagedetails.html',{"images":images})
 
-@login_required(login_url='/accounts/login/')
-def follow(request,user_id):
-    users = User.objects.get(id=user_id)
-    follow = Follow.objects.add_follower(request.user,users)
-    return render(request,'profile/profile.html',{"users":users,"follow":follow})
-
-#
 # @login_required(login_url='/accounts/login/')
 # def search_results(request):
 #     if 'query' in request.GET and request.GET['query']:
